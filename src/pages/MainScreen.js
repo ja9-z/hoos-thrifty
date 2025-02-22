d033af732f88cc973ba90aea9116e13826066f17
abai816 <baibaiannie@gmail.com>
2025-02-22 13:29:54 -0500

    created pages

---
+++ src/pages/MainScreen.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header"
+
+export default function MainScreen(){
+    return(
+        <div>
+            <Header/>
+            <h2>Begin dating! Meet the bacheloers</h2>
+        </div>
+    )
+}
---
+++ src/pages/Welcome.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header"
+
+export default function Welcome(){
+    return(
+        <div>
+            <Header/>
+            <h2>Welcome page</h2>
+        </div>
+    )
+}
