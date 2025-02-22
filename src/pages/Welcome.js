3cf66677583905ada03734c9e3c54a8310d3ee8b
abai816 <baibaiannie@gmail.com>
2025-02-22 14:36:54 -0500

    edited welcome page to add button

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,38 @@
 import Header from "../components/Header"
+import {useNavigate} from "react-router-dom"
 
 export default function Welcome(){
+    const nagivate = useNavigate()
     return(
-        <div>
-            <Header/>
-            <h2>Welcome page</h2>
+        <div class = 'welcome'
+        style = {{
+            margin: "auto",
+            width: 1000,
+            
+        }}
+        >
+            <h1
+                style = {{
+                    textAlign: "center",
+                    marginTop:250,
+                    marginBottom: 225,
+                    fontSize: 120,
+                    
+
+                }}
+            >HOOS THRIFTY</h1>
+            <button 
+                onClick={() => nagivate("main")}
+                style = {{
+                    display: "block",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    fontSize: 40,
+                    marginBottom: "auto"
+                }
+                }
+            > 
+            Get started 💖 </button>
         </div>
     )
 }
