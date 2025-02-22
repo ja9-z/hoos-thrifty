59871588f612ee4697d25b1d2ed76bc62303a2ab
ja9-z <janine.zeng.6@gmail.com>
2025-02-22 16:52:22 -0500

    annie on da big mac

--- src/pages/MainScreen.js
+++ src/pages/MainScreen.js
@@ -8,10 +8,18 @@ import {Card} from "../components/Card"
 export default function MainScreen(){
     return(
         <div className = "MainScreen">
-            <Card imgSrc = "https://picsum.photos/id/237/200/300" 
-            imgAlt = "Selling" title = "Items for Sale" 
-            description = "beautiful dog looking to be adopted. Not much to look for aside from here, brand new, 11 years old."/>
+            <div>
+                <h1 style = {{
+                    textAlign: "center",
+                    fontSize: 50,
+                    fontFamily: "Playfair Display"
+                }}>meet your match!💞</h1>
+            </div>
+            <div className = "subScreen">
+                <Card imgSrc = "https://picsum.photos/id/237/200/300" 
+                imgAlt = "Selling" title = "Items for Sale" 
+                description = "beautiful dog looking to be adopted. Not much to look for aside from here, brand new, 11 years old."/>
+            </div>
         </div>
-
     )
 }
