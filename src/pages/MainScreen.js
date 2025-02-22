f60c63048b8059517b45661297e20c67fd5843f3
ja9-z <janine.zeng.6@gmail.com>
2025-02-22 14:42:05 -0500

    started mainscreen

--- src/pages/MainScreen.js
+++ src/pages/MainScreen.js
@@ -1,10 +1,16 @@
-import Header from "../components/Header"
+//import Header from "../components/Header"
+
+
+//let testItem = new itemProfile("doe", "jane", "hello@email", "itemname");
+
+
 
 export default function MainScreen(){
     return(
         <div>
-            <Header/>
+            
             <h2>Begin dating! Meet the bacheloers</h2>
+            
         </div>
     )
 }
