a897cd4ef63684926f6fd4cc621f16fbeab41b4c
hailey chong <haileychongedu@gmail.com>
2025-02-23 13:38:03 -0500

    added more to landing page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import image1 from "../assets/image.png";
+import image2 from "../assets/image2.png";
+import explain from "../assets/Explain.png";
+
+
 import { motion, useScroll, useTransform } from 'framer-motion'; // Import Framer Motion
 import './Welcome.css';
 
@@ -12,7 +16,7 @@ const Welcome =()=> {
     const y = useTransform(scrollYProgress, [0, 1], [0, -2000]); // Adjust translation for scroll
 
     return (
-        <div
+        <><div
             className="welcome"
             style={{
                 margin: "auto",
@@ -21,21 +25,21 @@ const Welcome =()=> {
                 backgroundColor: "#FFFDF7",
                 paddingBottom: "200vh",
             }}
-        >   
+        >
             <motion.div whileHover={{ backgroundColor: "rgba(0, 0, 0, 1)" }}>
-            <h1
-                style={{
-                    textAlign: "center",
-                    marginTop: 200,
-                    marginBottom: 225,
-                    fontSize: 150,
-                    fontFamily: "Playfair Display",
-                    fontWeight: "normal",
-                    position: "relative",
-                }}
-            >
-                HOOS THRIFTY
-            </h1>
+                <h1
+                    style={{
+                        textAlign: "center",
+                        marginTop: 200,
+                        marginBottom: 225,
+                        fontSize: 150,
+                        fontFamily: "Playfair Display",
+                        fontWeight: "normal",
+                        position: "relative",
+                    }}
+                >
+                    HOOS THRIFTY
+                </h1>
             </motion.div>
 
             {/* Wrap image with Framer Motion's motion.img */}
@@ -44,7 +48,7 @@ const Welcome =()=> {
                     src={image1}
                     className="animated-image"
                     alt="image.png"
-                    initial={{ opacity: 0}}
+                    initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
                     transition={{ duration: 2 }}
                     style={{
@@ -53,57 +57,141 @@ const Welcome =()=> {
                         margin: '0 10px',
                         y, // Apply the scroll-based translation
                         position: 'relative',
-                        top: '-580px',
-                    }}
-                />
+                        top: '-550px',
+                    }} />
             </div>
 
-<div className="button-container" style={{ display: "flex" }}>
-    <motion.button
-        onClick={() => navigate("/seller")}
-        initial={{ opacity: 0 }}
-        whileHover={{ backgroundColor: "rgba(220, 220, 220, 1)" }}
-        whileTap={{ backgroundColor: "rgba(255, 255, 255, 1)" }}
-        whileInView={{ opacity: 1 }}
-        style={{
-            display: "block",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginTop: -100,
-            fontSize: 40,
-            fontFamily: "Playfair Display",
-            borderRadius: 35,
-            position: "relative", 
-            top: -650,
-            left: 250,
-        }}
-    >
-        sell something💸
-    </motion.button>
-    <motion.button
-        onClick={() => navigate("/main")}
-        initial={{ opacity: 0 }}
-        whileHover={{ backgroundColor: "rgba(220, 220, 220, 1)" }}
-        whileTap={{ backgroundColor: "rgba(255, 255, 255, 1)" }}
-        whileInView={{ opacity: 1 }}
-        style={{
-            display: "block",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginTop: -100,
-            fontSize: 40,
-            fontFamily: "Playfair Display",
-            borderRadius: 35,
-            position: "relative",
-            top: -650,
-            left: -200, 
-        }}
-    >   
-        get started🩷 
-    </motion.button>
-</div>
+            <div className="image-container2">
+                <motion.img
+                    src={image2}
+                    className="animated-image2"
+                    alt="image2.png"
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 2 }}
+                    style={{
+                        width: '1500px',
+                        position: 'absolute',
+                        y, // Apply the scroll-based translation
+                        top: '160%',
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: 1,
+                    }} />
+            </div>
+
+            <div className="button-container" style={{ display: "flex" }}>
+                <motion.button
+                    onClick={() => navigate("/seller")}
+                    initial={{ opacity: 0 }}
+                    whileHover={{ backgroundColor: "rgba(220, 220, 220, 1)" }}
+                    whileTap={{ backgroundColor: "rgba(255, 255, 255, 1)" }}
+                    whileInView={{ opacity: 1 }}
+                    style={{
+                        display: "block",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        marginTop: -100,
+                        fontSize: 40,
+                        fontFamily: "Playfair Display",
+                        borderRadius: 35,
+                        position: "relative",
+                        top: -650,
+                        left: 250,
+                    }}
+                >
+
+                    sell something💸
+                </motion.button>
+                <motion.button
+                    onClick={() => navigate("/main")}
+                    initial={{ opacity: 0 }}
+                    whileHover={{ backgroundColor: "rgba(220, 220, 220, 1)" }}
+                    whileTap={{ backgroundColor: "rgba(255, 255, 255, 1)" }}
+                    whileInView={{ opacity: 1 }}
+                    style={{
+                        display: "block",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        marginTop: -100,
+                        fontSize: 40,
+                        fontFamily: "Playfair Display",
+                        borderRadius: 35,
+                        position: "relative",
+                        top: -650,
+                        left: -200,
+                    }}
+                >
+                    get started🩷
+                </motion.button>
+
+            </div>
+            <motion.div
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 2 }}
+                viewport={{ once: true, amount: 0.8 }}
+            >
+                <h2
+                    style={{
+                        textAlign: "center",
+                        marginTop: -260,
+                        left: -200,
+                        marginBottom: 225,
+                        fontSize: 60,
+                        fontFamily: "Montserrat",
+                        fontWeight: "normal",
+                        position: "relative",
+                    }}
+                >
+                    cat break your favorite mug? ☕<br />
+                    in need of a new cup...and rug?
+                </h2>
+            </motion.div>
+
+            <motion.div
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 3 }}
+                viewport={{ once: true, amount: 1 }}
+            >
+                <h2
+                    style={{
+                        textAlign: "right",
+                        marginTop: 180,
+                        left: -200,
+                        marginBottom: 225,
+                        fontSize: 60,
+                        fontFamily: "Montserrat",
+                        fontWeight: "normal",
+                        position: "relative",
+                    }}
+                >
+                    new to the city? 🏙️<br />
+                    in need of new furniture?
+                </h2>
+            </motion.div>
         </div>
+        <div className="image-containerExplain">
+                <motion.img
+                    src={explain}
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 2 }}
+                    className="animated-imageExplain"
+                    alt="Explain.png" 
+                    style={{
+                        width: '1400px',
+                        position: 'relative',
+                         // Apply the scroll-based translation
+                        top: -1600,
+                        left: 760,
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: 1,
+                    }} />
+            </div></>
+        
     );
+    
 }
 
 export default transition(Welcome);
