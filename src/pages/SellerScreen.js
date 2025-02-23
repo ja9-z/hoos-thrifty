75a8c4c93b93d6c31a8f55fa3db19c93b7dc0222
ja9-z <janine.zeng.6@gmail.com>
2025-02-23 11:43:49 -0500

    changed caps

--- src/pages/SellerScreen.js
+++ src/pages/SellerScreen.js
@@ -65,21 +65,21 @@ return (
         </div>
         </div>
         <div class = "main-container">
-            <h1 class = "header">One last step...</h1>
+            <h1 class = "header">one last step...</h1>
         <div class = "form">
-            <h1 class = "formHeader">Finish these sentences to find the one 😉</h1>
+            <h1 class = "formHeader">finish these sentences to find the one 😉</h1>
             <form>
-                <label class = "question1Label">Q1. Together we could...</label>
+                <label class = "question1Label">together we could...</label>
                 <textarea class = "question1"
                     required
                 >
                 </textarea>
-                <label class = "question2Label">Q2. You could take me home if you...</label>
+                <label class = "question2Label">you could take me home if you...</label>
                 <textarea class ="question2"
                     required
                 >
                 </textarea>
-                <label class = "question3Label">Q3. Some of my imperfections are...</label>
+                <label class = "question3Label">some of my imperfections are...</label>
                 <textarea class ="question3"
                     required
                 >
